dc7a573e56c4b035955dae8ee116340b5355321b
pawan-sharma3110 <pawansharma93520@gmail.com>
2024-03-14 20:42:10 +0530

    some changes

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 // alert("Hello World");
 // Making a Object type varible
 const detailSetion = {
-    name  : "Pawan",
-    mobileNumber   : 7988323110,
-    age  : 21,
-    address : "Kheri"
+  name: "Pawan",
+  mobileNumber: 7988323110,
+  age: 21,
+  address: "Kheri",
 };
 
-const Profile ={
-    userName : "Pawan",
-    post : 5 +"K",
-    folowers : 3 +"K"
-}
+const Profile = {
+  userName: "Pawan",
+  post: 5 + "K",
+  folowers: 3 + "K",
+};
 console.log(Profile);
