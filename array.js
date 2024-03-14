d0cf1d9dcd3b889ea71760c81d318d00d3d367e8
pawan-sharma3110 <pawansharma93520@gmail.com>
2024-03-15 01:59:13 +0530

    array compete

---
+++ array.js
@@ -0,0 +1,26 @@
+// let marks = [85, 97, 44, 37, 76, 60];
+// let sum = 0;
+// for (let value of marks) {
+//   sum = sum + value;
+// }
+
+// let avrageNum = sum / 6;
+// console.log(avrageNum);
+
+// let prizeList = [250, 645, 300, 900, 50];
+// for (let i = 0; i < prizeList.length; i++) {
+//   let newPrize = [prizeList[i] - prizeList[i] / 10];
+//   console.log(newPrize);
+// }
+
+let companyName = [
+  "Bloomberg",
+  "Microsoft",
+  "Uber",
+  "Google",
+  "IBM",
+  "Netflix",
+];
+
+companyName.push("Amezone");
+console.log(companyName);
---
+++ string.js
@@ -0,0 +1,16 @@
+// tampleate string
+let p = "Pawan";
+let s = "Sharma";
+let n = "Pawan\tSharma";
+console.log(`My first name is ${p} \t and my last name is ${s}`);
+console.log(n.length);
+
+// methords
+
+let low = "hello world";
+let up = low.toUpperCase();
+console.log(up);
+
+var na = "Pawan";
+var userName = n.concat("@" + na.length);
+console.log(userName);
