476a7759690aae630df5c4dcc5ae2ad5a788d2e2
pawan-sharma3110 <pawansharma93520@gmail.com>
2024-03-15 23:58:58 +0530

    arrow function complete

--- function.js
+++ function.js
@@ -1,41 +1,41 @@
-const countVowel = (word)=> {
-  let count = 0;
-  for (const char of word) {
-    if (
-      char === "a" ||
-      char === "e" ||
-      char === "i" ||
-      char === "o" ||
-      char === "u"
-    ) {
-      count++;
-      console.log(char);
-    }
-  }
-  console.log(count);
-}
-countVowel("aeiou");
+// const countVowel = (word)=> {
+//   let count = 0;
+//   for (const char of word) {
+//     if (
+//       char === "a" ||
+//       char === "e" ||
+//       char === "i" ||
+//       char === "o" ||
+//       char === "u"
+//     ) {
+//       count++;
+//       console.log(char);
+//     }
+//   }
+//   console.log(count);
+// }
+// countVowel("aeiou");
 
-let num = [1, 2, 3, 4, 5, 6, 7];
+// let num = [1, 2, 3, 4, 5, 6, 7];
 
-num.forEach((num, ind) => {
-  let result = num * num;
-  console.log(result, ind);
-});
+// num.forEach((num, ind) => {
+//   let result = num * num;
+//   console.log(result, ind);
+// });
 
-let marks = [89, 99, 92, 56, 14, 63, 95];
+// let marks = [89, 99, 92, 56, 14, 63, 95];
 
-const choose = marks.filter((h) => {
-  return h > 90;
-});
-console.log(choose);
+// const choose = marks.filter((h) => {
+//   return h > 90;
+// });
+// console.log(choose);
 
-function makeArr(vel) {
-  let arr = [];
-  let newArr = arr.push(vel);
-  console.log(newArr[vel]);
-}
-makeArr("pawan");
+// function makeArr(vel) {
+//   let arr = [];
+//   let newArr = arr.push(vel);
+//   console.log(newArr[vel]);
+// }
+// makeArr("pawan");
 let aar = [];
 
 function makeArr(n) {
@@ -44,9 +44,9 @@ function makeArr(n) {
   }
   return console.log(aar);
 }
-makeArr(5);
+makeArr(50);
 let sum = aar.reduce((res, curr) => {
-  return res + curr;
+  return res * curr;
 });
 
 console.log(sum);
